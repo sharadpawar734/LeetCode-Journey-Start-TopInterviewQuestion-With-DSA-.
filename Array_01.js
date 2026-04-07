7ebd916dd1d6a7cbc6405b1b0e18141b1228926a
Sharad Pawar <sharadpawar12003@gmail.com>
2026-04-07 16:47:30 +0530

    Add files via upload

---
+++ Array_01.js
@@ -0,0 +1,8 @@
+let marks_class = [30,20,11,9,44, 60,80,20, null]
+console.log(marks_class)
+marks_class [5] = 99;       //update marks
+console.log(marks_class[0])
+console.log(marks_class[2])
+console.log(marks_class.length)
+console.log(marks_class[5])
+console.log(typeof marks_class)
